e0984214ac59db1c7d4b979fca54054c10ae8c59
GUSTAVO HENRIQUE DA SILVA <22282186@CFP127ALUNO.LOCAL>
2023-02-13 11:21:47 -0300

    Finalizados todos os exercicio e realizado atualização. Aula do dia 13/02/2023

---
+++ Aula07ExercicioCalculadoraAtualizacao/modulo/calculadora.js
@@ -0,0 +1,77 @@
+/*****************************************************************************************
+ * Objetivo: Realizar o back-end para realizar os calculos da tabuada e validação de dados
+ * Data: 09/02/2023
+ * Autor: GustavoHenrique
+ * Versão: 1.0
+ *****************************************************************************************/
+
+//Retorna a validação da tabuada para verificar se ela esta entre 2 a 100 e se não são iguais
+const validacaoTabuada = function ($tabuadaInicial, $tabuadaFinal) {
+
+    let tabuadaInicial = Number($tabuadaInicial)
+    let tabuadaFinal = Number($tabuadaFinal)
+    let statusValidacao = true
+
+    if ((tabuadaInicial < 2 || tabuadaInicial > 100) || (tabuadaFinal < 2 || tabuadaFinal > 100)) {
+        console.log('Atenção: A entrada de dados devera ficar entre 2 e 100')
+        statusValidacao = false
+    } else if (tabuadaInicial == tabuadaFinal) {
+        console.log('Atenção a tabuada inicial e final não podem ser iguais.')
+        statusValidacao = false
+    } else {
+        statusValidacao = true
+    }
+
+    return statusValidacao
+}
+
+//Verifica o multiplicando max e min está entre 1 a 50
+const maxMinTabuada = function ($tabuadaInicialMultiplicador, $tabuadaFinalMultiplicador) {
+
+    let tabuadaInicialMultiplicador = Number($tabuadaInicialMultiplicador)
+    let tabuadaFinalMultiplicador = Number($tabuadaFinalMultiplicador)
+    let statusMaxMin = true
+
+    if ((tabuadaInicialMultiplicador < 1 || tabuadaInicialMultiplicador > 50) || (tabuadaFinalMultiplicador < 1 || tabuadaFinalMultiplicador > 50)) {
+
+        console.log('O valor onde será calculado a tabuada deverá ficar entre 1 e 50')
+        return statusMaxMin = false
+
+    } else {
+        return statusMaxMin
+    }
+
+}
+
+//Função responsavel por realizar o calculo da tabuada
+const calculoTabuada = function ($tabuadaInicial, $tabuadaFinal, $tabuadaInicialMultiplicador, $tabuadaFinalMultiplicador) {
+    let tabuadaInicialMultiplicador = Number($tabuadaInicialMultiplicador)
+    let tabuadaFinalMultiplicador = Number($tabuadaFinalMultiplicador)
+    let tabuadaInicial = Number($tabuadaInicial)
+    let tabuadaFinal = Number($tabuadaFinal)
+
+    //For responsavel por Defenir o multiplicador de toda a tabuada inicial ate a tabuada final(Multiplicador)
+    for (let tabuadaContinuacao = tabuadaInicial; tabuadaContinuacao <= tabuadaFinal; tabuadaContinuacao++) {
+
+        //for responsavel por mostra o calculo de todas as tabuadas
+        for (let multiplicando = tabuadaInicialMultiplicador; multiplicando <= tabuadaFinalMultiplicador; multiplicando++) {
+
+            produto = tabuadaContinuacao * multiplicando
+            console.log(`${tabuadaContinuacao} X ${multiplicando} = ${produto}`)
+
+        }
+        console.log('--------------------------------')
+
+    }
+
+
+
+}
+
+//Exportando funções
+
+module.exports = {
+    validacaoTabuada,
+    maxMinTabuada,
+    calculoTabuada
+}
